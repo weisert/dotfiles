c95bddd6913c4386fb80c10bd5f383537a741fdc
Igor Vayzert <igor.weisert@gmail.com>
2015-10-17 20:23:45 +0600

    Create draft of browsertest.

---
+++ create-draft.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# Copyright (c) 2015 Igor Vayzert
+
+import argparse
+import datetime
+import os
+import re
+import subprocess
+import sys
+
+HEADERS = ['// Copyright (c) {year} {company} LLC. All rights reserved.',
+           '// Author: {name} <{mail}>']
+
+
+def create_headers():
+    data = {'year': str(datetime.date.today().year),
+            'company': raw_input('Please enter your company: '),
+            'name': subprocess.check_output(
+                ['git', 'config', 'user.name']).strip(),
+            'mail': subprocess.check_output(
+                ['git', 'config', 'user.email']).strip()}
+    result = []
+    for line in HEADERS:
+        result.append(line.format(**data))
+    return result
+
+
+def check_cwd():
+    if os.path.basename(os.getcwd()) != 'src':
+        print 'Your current directory must be \"src\".'
+        return False
+    return True
+
+
+def parse_command_line_options(argv):
+    parser = argparse.ArgumentParser(description='Create something in browser.')
+    parser.add_argument('-w', '--what',metavar='WHAT', type=str.lower,
+                        help='What to create: "class", "header", "source", "browsertest", "unittest"',
+                        choices=['class', 'header', 'source', 'browsertest', 'unittest'])
+    parser.add_argument('path', nargs='?', help='Full path.', default=None, type=str)
+    if len(argv) == 0:
+        parser.print_help()
+    return parser.parse_args(argv)
+
+
+def create_class(path):
+
+    print "create class here", path
+
+
+def create_browser_test(path):
+    lines = create_headers()
+    lines.append('')
+    included_file = path.replace('_browsertest.cc', '.h')
+    lines.append('#include "' + included_file + '"')
+    lines.append('')
+    lines.append('#include "chrome/test/base/in_process_browser_test.h"')
+    lines.append('')
+
+    class_name = os.path.basename(included_file).replace('.h', '')
+
+    def to_upper(patern):
+        s = patern.group(1)
+        if s.startswith('_'):
+            return s[1:].upper()
+        return s.upper()
+    class_name = re.sub(r'(^.|_.)', to_upper, class_name) + 'Test'
+
+    lines.append('class ' + class_name + ' : public InProcessBrowserTest {')
+    lines.append('};')
+    lines.append('')
+    lines.append('IN_PROC_BROWSER_TEST_F(' + class_name +', Draft) {')
+    lines.append('')
+    lines.append('}')
+    lines.append('')
+
+    filename = os.path.join(os.getcwd(), path)
+    with open(filename, 'w') as f:
+        f.write('\n'.join(lines))
+
+
+def main(argv):
+    if not check_cwd():
+        return 1
+    options = parse_command_line_options(argv[1:])
+    if options.what == 'class':
+        create_class(options.path)
+    if options.what == 'browsertest':
+        create_browser_test(options.path)
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main(sys.argv))
