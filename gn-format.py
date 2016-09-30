c9c8da0cbb93a9eb8709e2586d55ac7e4e36abac
Igor Vayzert <igor.weisert@gmail.com>
2016-09-30 13:28:09 +0700

    Ability to format the whole tree of gn files

--- gn-format.py
+++ gn-format.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python
 
+import sys
 from subprocess import check_output, check_call
 
-output = check_output(['git', 'ls-files', '-m'])
-files = [x for x in output.split('\n') if x.endswith('BUILD.gn')]
+
+def format_gn_file(filename):
+    check_call(['gn', 'format', filename])
+
+
+def get_modified_gn_files():
+    output = check_output(['git', 'ls-files', '-m'])
+    return [x for x in output.split('\n') if x.endswith('BUILD.gn')]
+
+
+def get_all_gn_files():
+    output = check_output(['git', 'ls-files'])
+    return [x for x in output.split('\n') if x.endswith('BUILD.gn')]
+
+
+if len(sys.argv) == 1:
+    files = get_modified_gn_files()
+elif len(sys.argv) == 2 and sys.argv[1] == 'all':
+    files = get_all_gn_files()
+else:
+    print 'Usage: ' + sys.argv[0] + '[all]'
+    exit(1)
+
 for f in files:
-    check_call(['gn', 'format', f])
+    format_gn_file(f)
