96733f7565420da994a179ed13fb347739c48e49
Igor Vayzert <igor.weisert@gmail.com>
2016-10-18 13:25:10 +0600

    Generator for args.gn files.

---
+++ ggn.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+
+if os.path.basename(os.getcwd()) != 'src':
+    print 'Your current directory must be \'src\'.'
+    exit(1)
+
+gn_paths = [os.path.join(os.getcwd(), 'out', 'debug_gn'), os.path.join(os.getcwd(), 'out', 'release_gn')]
+
+for p in gn_paths:
+    if not os.path.exists(p):
+        os.makedirs(p, 755)
+    with open(os.path.join(p, 'args.gn'), 'w') as f:
+        f.write('dcheck_always_on = true\n')
+        f.write('is_component_build = true\n')
+        f.write('enable_nacl = false\n')
+        if os.path.basename(p) == 'debug_gn':
+            f.write('is_debug = true\n')
+        else:
+            f.write('is_debug = false\n')
